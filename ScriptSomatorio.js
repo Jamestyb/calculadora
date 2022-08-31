e1bbed15f50935fac09209b16b1a6fa16b9f35ce
Jamestyb <108416016+Jamestyb@users.noreply.github.com>
2022-08-31 12:13:51 -0300

    Calculadora  de soma

--- ScriptSomatorio.js
+++ ScriptSomatorio.js
@@ -1,1 +1,11 @@
-alert("Calculadora Soma de alunos");
+function Calsom(){
+
+let num1 = document.getElementsByName("num1")[0].value;
+let num2 = document.getElementsByName("num2")[0].value;
+let sum = Number(num2) + Number(num1);
+document.getElementsByName("somado")[0].value = sum;
+
+
+    
+}
+
