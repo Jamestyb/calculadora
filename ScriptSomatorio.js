146108f15e5e0ff57fb94708e3e34f19e5fb61a0
Jamestyb <108416016+Jamestyb@users.noreply.github.com>
2022-09-01 10:21:00 -0300

    Add files via upload

--- ScriptSomatorio.js
+++ ScriptSomatorio.js
@@ -4,8 +4,5 @@ let num1 = document.getElementsByName("num1")[0].value;
 let num2 = document.getElementsByName("num2")[0].value;
 let sum = Number(num2) + Number(num1);
 document.getElementsByName("somado")[0].value = sum;
-
-
-    
 }
 
